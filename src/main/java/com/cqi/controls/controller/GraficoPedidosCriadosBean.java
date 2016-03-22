9c57818203c4d86e15952db74d781f62ef4df3e1
CQFB <cleoquintiliano@gmail.com>
2016-03-21 21:07:15 -0300

    Inclusão de gráfico de linhas

---
+++ src/main/java/com/cqi/controls/controller/GraficoPedidosCriadosBean.java
@@ -0,0 +1,44 @@
+package com.cqi.controls.controller;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+
+/**
+ * 
+ * @author cqfb
+ *
+ */
+
+@Named
+@RequestScoped
+public class GraficoPedidosCriadosBean {
+	
+	private CartesianChartModel model;
+
+	public void preRender() {
+		this.model = new CartesianChartModel();
+		
+		adicionarSerie("Todos os pedidos");
+		adicionarSerie("Meus pedidos");
+	}
+	
+	private void adicionarSerie(String rotulo) {
+		ChartSeries series = new ChartSeries(rotulo);
+		
+		series.set("1", Math.random() * 1000);
+		series.set("2", Math.random() * 1000);
+		series.set("3", Math.random() * 1000);
+		series.set("4", Math.random() * 1000);
+		series.set("5", Math.random() * 1000);
+		
+		this.model.addSeries(series);
+	}
+
+	public CartesianChartModel getModel() {
+		return model;
+	}
+
+}
